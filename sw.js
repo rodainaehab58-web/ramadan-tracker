394a065cdc1b74bd42e408dbd9550b409c4054f1
rodainaehab58-web <rodaina.ehab58@gmail.com>
2026-02-21 09:56:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const daySelector = document.getElementById('daySelector');
+
 self.addEventListener("install", function(event) {
   console.log("Service Worker Installed");
 });
+});
+
+
